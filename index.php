3986598e43419c6102add4b0a8d94d517798983c
zinevichvi <zinevichvi@git.oxagile.com>
2019-05-13 16:34:16 +0300

    add any function

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$mainData = [1,7,4,3];
+$mainData = [1, 7, 4, 3];
 $someData = [
     ['x' => 1, 'y' => 2],
     ['x' => 5, 'y' => 7],
@@ -8,15 +8,14 @@ $someData = [
 ];
 
 //array_column
-
-print_r(array_column($someData, 'x'));
-
+$result = array_column($someData, 'x');
+print_r($result);
 
 //array_map
 
 function showMeArray($val)
 {
-    return("Элемент $val");
+    return ("Элемент $val");
 }
 
 foreach ($someData as $data) {
@@ -24,12 +23,15 @@ foreach ($someData as $data) {
     print_r($newData);
 }
 
-print_r(array_map("showMeArray", $mainData));
+$result = array_map("showMeArray", $mainData);
+print_r($result);
 
 //array_chunk
 
-print_r(array_chunk($someData, 2));
-print_r(array_chunk($someData, 2, true));
+$result = array_chunk($someData, 2);
+print_r($result);
+$result = array_chunk($someData, 2, true);
+print_r($result);
 
 //array_merge
 
@@ -47,5 +49,27 @@ print_r($result);
 
 //array_multisort
 
-array_multisort($mainData, SORT_NUMERIC, SORT_DESC);
-var_dump($mainData);
+$result = array_multisort($mainData, SORT_NUMERIC, SORT_DESC);
+var_dump($result);
+
+//array_pad
+
+$result = array_pad($mainData, 9, 0);
+print_r($result);
+
+//array_product
+
+$result = array_product($mainData) . PHP_EOL;
+echo $result;
+
+//array_replace
+
+$result = array_replace($someData, $mainData);
+print_r($result);
+
+//array_reverse
+
+$result = array_reverse($mainData); //not work
+print_r($mainData);
+$result = array_reverse($mainData, true);
+print_r($result);
